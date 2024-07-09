dcb1ba134a64c80f59cf5837a9a966afd49ceea5
Sasha <skalitsenia@mail.ru>
2024-07-09 22:16:12 +0300

    feat/abb Authorization key

--- src/api/api.ts
+++ src/api/api.ts
@@ -4,5 +4,8 @@ import axios from "axios";
 export const instance = axios.create({
     baseURL: 'https://social-network.samuraijs.com/api/1.0/',
     withCredentials: true,
-    headers: {'API-KEY': '02801dc9-c643-40b7-9ded-224fb486763d'}
+    headers: {
+        'API-KEY': '02801dc9-c643-40b7-9ded-224fb486763d',
+        "Authorization": "Bearer 8bdba674-1a3d-4ea5-a543-f143be76efbd"
+    }
 })
